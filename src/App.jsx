d374bee38bde6d5ba3f79aa1c3cc319d412b1a91
Shantanu <shantanuchougule7595@gmail.com>
2025-03-06 16:21:51 +0530

    comit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Clock from './components/Clock';
+
+function App() {
+  
+    return (
+        <>  
+         <Clock/>
+        </>
+    )
+}
+
+
+export default App
